54f96191645b8ded676d62538d9ee6ed05370bb9
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-04 13:28:07 -0400

    jumbotrom

---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Data from '../Data';
+
+function Wrapper() {
+    return (
+    <main className='wrapper'>
+        <Jumbotron>
+            <h1>
+                Employee Directory
+            </h1>
+        </Jumbotron>
+        <Data />
+    </main>
+    )
+}
+
+export default Wrapper;
