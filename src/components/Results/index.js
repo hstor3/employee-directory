774d5ba2480b3557612caa98d2c127fbf580bbb8
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-04 17:26:40 -0400

    bday formatted

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import moment from 'moment';
+
+// const dateFormat = {result.dob.date}toDateString();
 
 function searchResults(props) {
     console.log(props.headers)
+    // let bday = this.props.dob.date;
+    // let cBday = (new Date(bday)).toString();
+    // let bday = moment( this.result.dob.date)
     return (
         // look up table order
         <table className="list-group search-results">
@@ -18,6 +24,10 @@ function searchResults(props) {
                     <td>
                         <img alt='employee' src={result.picture.medium} className='employee-img' />
                         <div src={result.name.first}>{result.name.first} {result.name.last}</div>
+                        <div src={result.email}>{result.email}</div>
+                        <div>{moment(result.dob.date).format('MMMM Do, YYYY')}</div>
+                        {/* <div>{bday}</div> */}
+                        <div>{result.cell}</div>
                     </td>
                 </tr>
             ))}
