d67b7a536d1d5f3145a4e6bc441f13275464500e
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-08 11:57:22 -0400

    able to sort first name alphabetically

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -4,43 +4,102 @@ import Search from '../Search';
 import API from '../../utils/API'
 
 class Data extends Component {
-    state={
-        users: [],
-        searchTerm: ''
+  state = {
+    users: [],
+    searchTerm: "",
+    sort: null,
+  };
+
+  results = [];
+
+  // headers = [
+  //     {name: 'image', width: '10px'},
+  //     {name: 'name', width: '20px'},
+  //     {name: 'email', width: '20px'},
+  //     {name: 'birthday', width: '20px'},
+  //     {name: 'phone', width: '20px'}
+  //     // {birthday phone}
+  // ]
+
+  handleSort() {
+    if (!this.state.sort) {
+      return;
     }
 
-    headers = [
-        {name: 'image', width: '10px'},
-        {name: 'name', width: '20px'},
-        {name: 'email', width: '20px'},
-        {name: 'birthday', width: '20px'},
-        {name: 'phone', width: '20px'}
-        // {birthday phone}
-    ]
+    let usersList = this.state.users;
+    usersList.sort(function (a, b) {
+    //   if (this.state.sort === "first") {
+        let x = a.name.first;
+        let y = b.name.first;
+        if (x < y) {
+          return -1;
+        }
+        if (x > y) {
+          return 1;
+        }
+        return 0;
+    //   }
+    //   if (this.state.sort === "last") {
+    //     let x = a.name.last;
+    //     let y = b.name.last;
+    //     if (x < y) {
+    //       return -1;
+    //     }
+    //     if (x > y) {
+    //       return 1;
+    //     }
+    //     return 0;
+    //   }
+    //   return 0;
+    });
+    this.setState({ users: usersList });
 
-    // handlesort funtion look at article
+  };
 
-    handleInputChange = (e) => {
-        console.log(e.target.value);
-        this.setState({searchTerm: e.target.value})
-        // filter over users here
+  // handlesort funtion look at article
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const filteredUsers = this.results.filter((user) =>
+      user.name.first.toLowerCase().includes(this.state.searchTerm)
+    );
+    if (filteredUsers.length) {
+      this.setState({ users: filteredUsers });
+    } else {
+      this.setState({ users: this.results });
     }
+  };
 
-    componentDidMount() {
-        API.getAPI().then(users => {
-            console.log(users)
-            this.setState({users: users.data.results}) 
-        })
-    }
+  handleInputChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ searchTerm: e.target.value.toLowerCase() });
+  };
 
-    render() {
-        return (
-            <div>
-                <Search handleInputChange={this.handleInputChange}/>
-                <Results users={this.state.users} headers={this.headers}/>
-            </div>
-        )
-    }
+  setSort = (e) => {
+    this.setState({ sort: e.target.value },
+        () => {
+            this.handleSort();
+        });
+  };
+
+  componentDidMount() {
+    API.getAPI().then((users) => {
+      this.results = users.data.results;
+      this.setState({ users: users.data.results });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Search
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+          handleSort={this.setSort}
+        />
+        <Results users={this.state.users} />
+      </div>
+    );
+  }
 }
 
 export default Data;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,30 +1,52 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function employeeSearch(props) {
     return (
-        <form className='search'>
-            <div className='form-group'>
-                <label htmlFor='employee-name'>Employee Name:</label>
-                <input
-                value={props.search}
-                onChange={props.handleInputChange}
-                name='employee'
-                list='employees'
-                type='text'
-                className='form-control'
-                placeholder="Search for an employee"
-                id='employee'
-                />
-                {/* <datalist id='employees'>
-                    {props.employees.map(employee => (
-                        <option value={employee} key={employee} />
-                    ))}
-                </datalist> */}
-                <button type='submit' onClick={props.handleFormSubmit} className='btn btn-success'>
-                    Search
-                </button>
-            </div>
-        </form>
+        <Navbar bg="light" expand="lg">
+            {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+
+                <form className='search'>
+                    <div className='form-group'>
+                        <label htmlFor='employee-name'>Employee Name:</label>
+                        <input
+                        value={props.search}
+                        onChange={props.handleInputChange}
+                        name='employee'
+                        list='employees'
+                        type='text'
+                        className='form-control'
+                        placeholder="Search for an employee"
+                        id='employee'
+                        />
+                        {/* <datalist id='employees'>
+                            {props.employees.map(employee => (
+                                <option value={employee} key={employee} />
+                            ))}
+                        </datalist> */}
+                        <button type='submit' onClick={props.handleFormSubmit} className='btn btn-success'>
+                            Search
+                        </button>
+                    </div>
+                </form>
+                <label>Sort On</label>
+                <input type="radio" name="sort" value="first" onClick={props.handleSort} />
+                <label>Sort Off</label>
+                <input type="radio" name="sort" value="last" onClick={props.handleSort} />
+                {/* <Nav className="mr-auto">
+                        <NavDropdown title="Sort By" id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={props.handleSort('name.first')}>First Name</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.12">Last Name</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">DOB</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Phone Number</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                </Nav> */}
+            </Navbar.Collapse>
+         </Navbar>
     );
 }
 
